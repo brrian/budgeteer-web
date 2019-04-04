01e15ddcc31dc26fca4e879a2cd15a4689c75ad7
Brian Wang <brian.wang@thomsonreuters.com>
2019-04-04 10:30:06 -0500

    Add update split functionality

--- src/features/transactions/components/UpdateSplitModal.tsx
+++ src/features/transactions/components/UpdateSplitModal.tsx
@@ -1,56 +1,66 @@
-import React, { SFC } from 'react';
+import React, { SFC, FormEvent } from 'react';
+import { useFormData } from '../../../global/hooks';
+import { Split } from '../../../pages/base/store';
 import { AppContext } from '../../app';
+import { CategorySelect } from '../../categorySelect';
 import { ConfirmationButton } from '../../confirmationButton';
 import { Modal } from '../../modal';
 
-interface UpdateSplit {}
+interface UpdateSplit {
+  pos: string;
+  split: Split;
+}
 
-const UpdateSplitModal: SFC<UpdateSplit> = () => {
+const UpdateSplitModal: SFC<UpdateSplit> = ({ pos, split }) => {
+  const { formData, onChange } = useFormData(split);
+
   return (
     <AppContext.Consumer>
-      {({ closeModal }) => (
-        <Modal closeModal={closeModal}>
-          <h3>Update split</h3>
-          <form>
-            <div className="field">
-              <label className="label">Description</label>
-              <div className="control">
-                <input className="input" type="text" />
-              </div>
-            </div>
-            <div className="field">
-              <label className="label">Category</label>
-              <div className="control is-expanded">
-                <div className="select is-fullwidth">
-                  <select>
-                    <option value="category">Category</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-            <div className="field">
-              <label className="label">Note</label>
-              <div className="control">
-                <input className="input" type="text" />
-              </div>
-            </div>
-            <div className="field is-grouped is-grouped-right">
-              <div className="control">
-                <ConfirmationButton
-                  classNames="is-text has-text-danger"
-                  handleClick={() => {
-                    console.log('delete');
-                  }}
-                  label="Delete"
+      {({ categories, closeModal, updateTransaction }) => {
+        const handleFormSubmit = (event: FormEvent) => {
+          event.preventDefault();
+
+          updateTransaction('split', pos, formData);
+
+          closeModal();
+        };
+
+        return (
+          <Modal closeModal={closeModal}>
+            <h3>Update split</h3>
+            <form onSubmit={handleFormSubmit}>
+              <div className="field">
+                <label className="label">Category</label>
+                <CategorySelect
+                  categories={categories}
+                  defaultValue={`${formData.categoryId}`}
+                  onChange={onChange}
                 />
               </div>
-              <div className="control">
-                <button className="button is-primary">Update</button>
+              <div className="field">
+                <label className="label">Note</label>
+                <div className="control">
+                  <input className="input" type="text" />
+                </div>
               </div>
-            </div>
-          </form>
-        </Modal>
-      )}
+              <div className="field is-grouped is-grouped-right">
+                <div className="control">
+                  <ConfirmationButton
+                    classNames="is-text has-text-danger"
+                    handleClick={() => {
+                      console.log('delete');
+                    }}
+                    label="Delete"
+                  />
+                </div>
+                <div className="control">
+                  <button className="button is-primary">Update</button>
+                </div>
+              </div>
+            </form>
+          </Modal>
+        );
+      }}
     </AppContext.Consumer>
   );
 };
